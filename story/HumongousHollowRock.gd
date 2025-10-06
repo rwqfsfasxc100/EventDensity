68901693668c225624d3d9ee2f4f2884260eeb31
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-10-06 10:04:06 -0700

    updated density function

--- story/HumongousHollowRock.gd
+++ story/HumongousHollowRock.gd
@@ -1,11 +1,12 @@
 extends "res://story/HumongousHollowRock.gd"
 
-export var density = 0.0
+export (PoolIntArray) var maxDensity = [1000,1000,1000,1000,1000]
 
 func canBeAt(pos):
-	var i = get_parent().getTargetDensityAt(pos)
-	if i > density:
-		breakpoint
-		return .canBeAt(pos)
-	else:
-		return false
+	breakpoint
+	var density = get_parent().getTargetDensityAt(pos)
+	Debug.l("Testing for density for %s at %s: %s / %s" % [name,pos,density, maxDensity])
+	for n in range(maxDensity.size()):
+		if density[n] > maxDensity[n]:
+			return false
+	return .canBeAt(pos)
